1758ec2b8d4b3a6f7139f87e01f261ca4c214535
Khairus <playsave23@gmail.com>
2024-12-03 21:28:18 +0700

    feat: add formatCurrency utility function in formatter.tsx

---
+++ utils/formatter.tsx
@@ -0,0 +1,9 @@
+export const formatCurrency = (
+  value: number,
+  currency: string = "USD"
+): string => {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency,
+  }).format(value);
+};
