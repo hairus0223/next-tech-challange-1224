3607f364cb40b9a3a9fb825f8cf857716218ec27
Khairus <playsave23@gmail.com>
2024-12-04 00:16:45 +0700

    feat: update Carousel component and add CarouselThumbnail component

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from "react";
 import Image from "next/image";
+import { CarouselThumbnail } from "./CarouselThumbnail";
 
 interface CarouselProps {
   images: string[];
@@ -22,43 +23,70 @@ const Carousel: React.FC<CarouselProps> = ({ images }) => {
     );
   };
 
+  const buttonClassName =
+    "h-full px-6 transition-all ease-in-out hover:scale-110 hover:text-black dark:hover:text-white flex items-center justify-center";
+
   return (
-    <div className="relative w-full max-w-lg mx-auto">
-      <div className="relative w-full h-0 pb-[56.25%]">
-        {" "}
-        {/* This ensures the aspect ratio */}
-        <Image
-          src={images[currentIndex]}
-          alt={`Product Image ${currentIndex + 1}`}
-          layout="fill"
-          objectFit="cover"
-          className="rounded"
-        />
+    <>
+      <div className="relative aspect-square h-full max-h-[550px] w-full overflow-hidden">
+        {images[currentIndex] && (
+          <Image
+            src={images[currentIndex]}
+            alt={`Product Image ${currentIndex + 1}`}
+            className="h-full w-full object-contain"
+            fill
+            sizes="(min-width: 1024px) 66vw, 100vw"
+          />
+        )}
+
+        {images.length > 1 ? (
+          <div className="absolute bottom-[15%] flex w-full justify-center">
+            <div className="mx-auto flex h-11 items-center rounded-full border border-white bg-neutral-50/80 text-neutral-500 backdrop-blur dark:border-black dark:bg-neutral-900/80">
+              <button
+                onClick={handlePrev}
+                aria-label="Previous product image"
+                className={buttonClassName}
+              >
+                &larr;
+              </button>
+              <div className="mx-1 h-6 w-px bg-neutral-500"></div>
+              <button
+                onClick={handleNext}
+                aria-label="Next product image"
+                className={buttonClassName}
+              >
+                &rarr;
+              </button>
+            </div>
+          </div>
+        ) : null}
       </div>
-      <button
-        onClick={handlePrev}
-        className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white rounded-full w-10 h-10 flex items-center justify-center focus:outline-none"
-      >
-        &larr;
-      </button>
-      <button
-        onClick={handleNext}
-        className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white rounded-full w-10 h-10 flex items-center justify-center focus:outline-none"
-      >
-        &rarr;
-      </button>
-      <div className="flex justify-center mt-2 space-x-2">
-        {images.map((_, index) => (
-          <button
-            key={index}
-            className={`w-3 h-3 rounded-full ${
-              index === currentIndex ? "bg-blue-500" : "bg-gray-300"
-            }`}
-            onClick={() => setCurrentIndex(index)}
-          ></button>
-        ))}
-      </div>
-    </div>
+      {images.length > 1 ? (
+        <ul className="my-12 flex items-center flex-wrap justify-center gap-2 overflow-auto py-1 lg:mb-0">
+          {images.map((image, index) => {
+            const isActive = index === currentIndex;
+
+            return (
+              <li key={image} className="h-20 w-20">
+                <button
+                  onClick={() => setCurrentIndex(index)}
+                  aria-label="Select product image"
+                  className="h-full w-full"
+                >
+                  <CarouselThumbnail
+                    alt={`Product Image ${index + 1}`}
+                    src={image}
+                    width={80}
+                    height={80}
+                    active={isActive}
+                  />
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      ) : null}
+    </>
   );
 };
 
---
+++ components/CarouselThumbnail.tsx
@@ -0,0 +1,56 @@
+import clsx from "clsx";
+import Image from "next/image";
+import ProductLabel from "./ProductLabel";
+
+interface LabelProps {
+  title: string;
+  amount: string;
+  currencyCode: string;
+  position?: "bottom" | "center";
+}
+
+interface CarouselThumbnailProps extends React.ComponentProps<typeof Image> {
+  isInteractive?: boolean;
+  active?: boolean;
+  label?: LabelProps;
+}
+
+export function CarouselThumbnail({
+  isInteractive = true,
+  active = false,
+  label,
+  ...props
+}: CarouselThumbnailProps) {
+  return (
+    <div
+      className={clsx(
+        "group flex h-full w-full items-center justify-center overflow-hidden rounded-lg border bg-white hover:border-blue-600 dark:bg-black",
+        {
+          relative: !!label,
+          "border-2 border-blue-600": active,
+          "border-neutral-200 dark:border-neutral-800": !active,
+        }
+      )}
+    >
+      {props.src && (
+        // eslint-disable-next-line jsx-a11y/alt-text
+        <Image
+          className={clsx(
+            "relative h-full w-full object-contain",
+            isInteractive &&
+              "transition duration-300 ease-in-out group-hover:scale-105"
+          )}
+          {...props}
+        />
+      )}
+      {label && (
+        <ProductLabel
+          title={label.title}
+          amount={label.amount}
+          currencyCode={label.currencyCode}
+          position={label.position}
+        />
+      )}
+    </div>
+  );
+}
